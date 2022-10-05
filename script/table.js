13faa5e9038c17742473d592dd359ede18007b0a
Matthew <mattshardlow@gmail.com>
2022-10-05 09:53:11 +0100

    bug fix

--- script/table.js
+++ script/table.js
@@ -1,32 +1,12 @@
 function table() 
 {
-  fetch("0-dummy.csv")
-  .then(res => res.text())
-  .then(csv => {
-    // (B1) REMOVE OLD TABLE ROWS
-    table.innerHTML = "";
-
-    // (B2) GENERATE TABLE
-    let rows = csv.split("\r\n");
-    for (let row of rows) {
-      let cols = row.match(/(?:\"([^\"]*(?:\"\"[^\"]*)*)\")|([^\",]+)/g);
-      if (cols!=null) {
-        let tr = table.insertRow();
-        for (let col of cols) {
-          let td = tr.insertCell();
-          td.innerHTML = col.replace(/(^"|"$)/g, "");
-        }//for
-      }//if
-    }//csv func
-  });//fetch
-
   var csv = require('jquery-csv');
 
-  fetch("Maste.csv")
+  fetch("Master.csv")
   .then(res => res.text())
   .then(csv => {
-    let master = $.csv.toArrays(csv);
-    console.log(master[0][0])
+    let master = $.csv.toObjects(csv);
+    console.log(master[0]['Group'])
   }//then
 
   
